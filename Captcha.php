1c9ca50fbd1e1dcb2b565c0cc0f9d5fa03445ded
Bergendahl94 <william_bergendahl@hotmail.com>
2014-05-18 15:36:37 +0200

    Creates Captcha script
    Creates a captcha to prevent bots from hacking.

---
+++ Captcha.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+//generates random number
+$code=rand(1000,9999);
+
+//stores the number in a session
+$_SESSION["code"]=$code;
+
+//creates an image
+$im = imagecreatetruecolor(50, 24);
+$bg = imagecolorallocate($im, 22, 86, 165); //background color blue
+$fg = imagecolorallocate($im, 255, 255, 255);//text color white
+
+//fills the image with the above properties
+imagefill($im, 0, 0, $bg);
+imagestring($im, 5, 5, 5,  $code, $fg);
+header("Cache-Control: no-cache, must-revalidate");
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
+?>
